9ccdd716fdbef410aa5a90eaaf9cfc812ea84376
Giulia Cajati <giuliacajati@gmail.com>
2020-05-27 14:56:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,23 @@ def using_concat (array1, array2)
 end 
 
 def using_insert (Array, element)
+  Arrya.insert(element)
+end 
+
+def using_uniq (Array)
+  .uniq 
+end
+
+def using_flatten (Array)
+  .flatten 
+end 
+
+def using_delete(Array, String)
+  .delete 
+end
+
+def using_delete_at(Array, Integer)
+  Array.delete_at(Integer)
+end 
+  
+  
